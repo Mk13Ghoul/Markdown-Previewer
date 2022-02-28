4fe54f5438eacd79c9bc2b100d741c5c3fe749c0
Mk13Ghoul <mayankkumarj5@gmail.com>
2022-02-28 14:56:26 +0530

    Updates

---
+++ precache-manifest.5ea2420fc15c1839ef16702c3b9161f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a5ec6659318ba6b730e",
+    "url": "/Markdown-Previewer/static/css/main.c7d83f6d.chunk.css"
+  },
+  {
+    "revision": "0a5ec6659318ba6b730e",
+    "url": "/Markdown-Previewer/static/js/main.0a5ec665.chunk.js"
+  },
+  {
+    "revision": "ebb01f1582699742545c",
+    "url": "/Markdown-Previewer/static/js/1.ebb01f15.chunk.js"
+  },
+  {
+    "revision": "1ef1a603158ce56d01ba",
+    "url": "/Markdown-Previewer/static/js/runtime~main.1ef1a603.js"
+  },
+  {
+    "revision": "a8f67e97b111f7c34aa89bc75624d91b",
+    "url": "/Markdown-Previewer/index.html"
+  }
+];
